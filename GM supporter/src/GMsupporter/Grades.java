54fb3bfbea4f21e4f3f3727e8426e6d6c512a3bf
sizu213 <shhmktooz@gmail.com>
2026-08-31 15:17:40 +0900

    データ

--- GM supporter/src/GMsupporter/Grades.java
+++ GM supporter/src/GMsupporter/Grades.java
@@ -1,5 +1,40 @@
 package GMsupporter;
 
 public class Grades {
+	private int id;
+	private String name;
+	private String subject;
+	private int score;
+
+	public Grades(int id, String name, String subject, int score) {
+		this.id = id;
+		this.name = name;
+		this.subject = subject;
+		this.score = score;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
 
 }
--- GM supporter/src/GMsupporter/GradesService.java
+++ GM supporter/src/GMsupporter/GradesService.java
@@ -1,5 +1,13 @@
 package GMsupporter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GradesService {
 
+	private List<Grades> gradesList = new ArrayList<>();
+
+	public void createGrades(int id, String name, String subject, int score) {
+
+	}
 }
