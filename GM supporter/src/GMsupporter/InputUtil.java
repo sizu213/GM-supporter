1a741383fd408c1d2ba4d586e6786d7642e1d256
sizu213 <shhmktooz@gmail.com>
2026-09-09 11:57:12 +0900

    データ

--- GM supporter/src/GMsupporter/InputUtil.java
+++ GM supporter/src/GMsupporter/InputUtil.java
@@ -7,8 +7,18 @@ public class InputUtil {
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static String readString(String prompt) {
-		System.out.println(prompt);
-		return scanner.nextLine();
+		while (true) {
+			System.out.println(prompt);
+
+			String input = scanner.nextLine();
+			if (input.matches(".*[0-9０-９].*")) {
+				System.out.println("数字は入力できません");
+				continue;
+			}
+			return input;
+
+		}
+
 	}
 
 	public static int readInt(String prompt) {
@@ -17,10 +27,18 @@ public class InputUtil {
 			if (scanner.hasNextInt()) {
 				int input = scanner.nextInt();
 				scanner.nextLine();
+
+				if (input < 0) {
+					System.out.println("マイナス値は入力できません");
+					continue;
+				}
 				return input;
 			} else {
-				System.out.println("エラー: 数字を入力してください");
+				System.out.println("数字を入力してください");
 				scanner.next();
+			}
+			{
+
 			}
 
 		}
